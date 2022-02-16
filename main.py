7f88f5ba02ed31410479c98853f8d933a172effc
BordovskyiPavlo <99017519+BordovskyiPavlo@users.noreply.github.com>
2022-02-16 15:46:27 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,56 @@
-from tkinter import *
-import random
-WIDTH = 500
-HEIGHT = 500
-Snake_size = 15
-root = Tk()
-c = Canvas(root , width =WIDTH, height= HEIGHT,background="black")
-c.pack()
-Snake = c.create_rectangle(WIDTH/2-Snake_size,HEIGHT/2-Snake_size,WIDTH/2+Snake_size,HEIGHT/2+Snake_size,fill="yellow")
-Snake_speed_x = 1
-Snake_speed_y = 1
-Snakes_speed = 10
-class Segment(object):
-    def __init__(self, x, y):
-        self.instance = c.create_rectangle(x, y,
-                         x+Snake_size, y+Snake_size,
-                         fill="white")
-class Sneak(object):
-    def __init__ (self,segments):
-        self.segments = segments
-    def move_snake(self):
-        SNAKES = {Snake: Snake_speed_x,Snake: Snake_speed_y}
-        for snake in SNAKES:
-            x= Snake_speed_x
-            y=Snake_speed_y
-            c.move(Snake,x,y)
-        if c.coords(snake)[1] <0:
-            c.move(snake,0,-c.coords(snake)[1])
-        elif c.coords(snake)[3] > HEIGHT:
-            c.move(Snake,0,HEIGHT - c.coords(snake)[3])
-        for index in range(len(self.segments) - 1):
-            segment = self.segments[index].instance
-            x1, y1, x2, y2 = c.coords(self.segments[index + 1].instance)
-            # задаем каждому сегменту позицию сегмента стоящего после него
-            c.coords(segment, x1, y1, x2, y2)
-    c.focus_set()
-
-
-
-    def movement_handler(self,event):
-        global Snake_speed_x,Snake_speed_y
-        if event.keysym == "w":
-            Snake_speed_y = -Snakes_speed
-        elif event.keysym == "s":
-            Snake_speed_y = Snakes_speed
-        elif event.keysym == "a":
-            Snake_speed_x = -Snakes_speed
-        elif event.keysym == "d":
-            Snake_speed_x = Snakes_speed
-    c.bind("<KeyPress>", movement_handler)
-    def stop_snake(event):
-        global Snakes_speed,Snake_speed_y,Snake_speed_x
-        if event.keysym in "a":
-            Snake_speed_y = 0
-        elif event.keysym in "w":
-            Snake_speed_x = 0
-        elif event.keysym in "s":
-            Snake_speed_x = 0
-        elif event.keysym in "d":
-            Snake_speed_y = 0
-    c.bind("<KeyRelease>", stop_snake)
-def main():
-    Snake.move_snake()
-    root.after(30, main)
-main()
-Snake.food()
-root.mainloop()
+import pygame , random , time
+pygame.init()
+pygame.display.set_caption("ping_pong")
+online = True
+player_racket = []
+bot_racket = []
+size = 5
+color1 = (255, 0, 0)
+color2 = (0, 255, 0)
+clock = pygame.time.Clock()
+class ping_pong:
+    def create_area(self,height, width):
+        self.gamescreen = pygame.display.set_mode((height,width))
+        self.background = pygame.Surface((height,width))
+        self.background.fill((0,0,0))
+    def create_player(self,cord_x,cord_y):
+        # player start x = 35 player start y = 250
+        player_racket=[(cord_x,cord_y)]
+        pygame.draw.rect(self.gamescreen, color1, (cord_x,cord_y,25,100))
+    def create_bot(self,cord_x,cord_y):
+        # bot start x = 740 bot start y = 250
+        pygame.draw.rect(self.gamescreen, color2, (740, 250, 25, 100))
+    def player_move(self,cord_x,cord_y):
+        self.gamescreen.blit(self.background, (0, 0))
+        pygame.draw.rect(self.gamescreen, color1, (cord_x, cord_y, 25, 100))
+        pygame.display.flip()
+if __name__ =='__main__':
+    player_cord_x = 35
+    bot_cord_x = 740
+    player_cord_y = 250
+    bot_cord_y = 250
+    K_w = pygame.K_w
+    K_s = pygame.K_s
+    p = ping_pong()
+    p.create_area(800, 600)
+    p.create_player(player_cord_x, player_cord_y)
+    p.create_bot(bot_cord_x, bot_cord_y)
+    p.player_move(35, 250)
+    while online:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                online = False
+        clock.tick(50)
+        pygame.display.flip()
+        keys = pygame.key.get_pressed()
+        if keys[K_w]:
+            move = (0, -5)
+            player_cord_y += move[1]
+            player_racket = [(player_cord_x, player_cord_y)]
+            p.player_move(player_cord_x, player_cord_y)
+        elif keys[K_s]:
+            move = (0, 5)
+            player_cord_y += move[1]
+            player_racket = [(player_cord_x, player_cord_y)]
+            p.player_move(player_cord_x, player_cord_y)
+        pygame.display.update()
