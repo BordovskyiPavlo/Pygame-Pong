8bea2fc5ce9048061f3b7a32a073860268b9c9f2
BordovskyiPavlo <99017519+BordovskyiPavlo@users.noreply.github.com>
2022-02-18 10:41:36 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import pygame , random , time
 pygame.init()
 pygame.display.set_caption("ping_pong")
 online = True
-player_racket = []
 bot_racket = []
 size = 5
 color1 = (255, 0, 0)
 color2 = (0, 255, 0)
 color3 = (255, 255, 0)
 clock = pygame.time.Clock()
+score = 0
+balls_move = (random.randint(-1, 1), random.randint(-1, 1))
+font = pygame.font.SysFont('serif', 25)
+text1 = font.render("Score: " + str(score), True, color1)
 class ping_pong:
     def create_area(self,height, width):
         self.gamescreen = pygame.display.set_mode((height,width))
@@ -16,24 +19,52 @@ class ping_pong:
         self.background.fill((0,0,0))
     def create_player(self,cord_x,cord_y):
         # player start x = 35 player start y = 250
-        player_racket=[(cord_x,cord_y)]
-        pygame.draw.rect(self.gamescreen, color1, (cord_x,cord_y,25,100))
+        # up wall 500 , down wall 0
+        self.cube = pygame.Surface((25,100))
+        self.cube.fill(color1)
+        self.player_racket = self.cube.get_rect()
+        self.player_racket.move_ip(35, 250)
+        self.gamescreen.blit(self.cube, self.player_racket)
+        pygame.display.update()
+    def create_ball(self, cord_x, cord_y):
+        self.balls = pygame.Rect(cord_x, cord_y, 25, 25)
+        pygame.draw.rect(self.gamescreen, color3, self.balls)
+    def ball_move(self, cord_x, cord_y):
+        pygame.Rect.move(self.balls, cord_x, cord_y)
+        pygame.display.flip()
     def create_bot(self,cord_x,cord_y):
         # bot start x = 740 bot start y = 250
-        pygame.draw.rect(self.gamescreen, color2, (740, 250, 25, 100))
+        self.cube2 = pygame.Surface((25, 100))
+        self.cube2.fill(color2)
+        self.bot_racket = self.cube2.get_rect()
+        self.bot_racket.move_ip(740, 250)
+        self.gamescreen.blit(self.cube2, self.bot_racket)
+        pygame.display.update()
+    def bot_move(self,x,y):
+        self.bmin_y = 250
+        self.bmax_y = 350
+        if y >self.bot_racket[1]:
+            self.bot_racket.move_ip(0, 1)
+        if y <self.bot_racket[1]:
+            self.bot_racket.move_ip(0, -1)
+        if self.bot_racket[1] >= 500:
+            self.bot_racket.move_ip(0, 0)
+        if self.bot_racket[1] <= 0:
+            self.bot_racket.move_ip(0, 0)
+
+        self.gamescreen.blit(self.cube2, self.bot_racket)
+        pygame.display.update()
     def player_move(self,cord_x,cord_y):
-        #up wall 500 , down wall 0
-        pygame.draw.rect(self.gamescreen, color1, (cord_x, cord_y, 25, 100))
-        pygame.display.flip()
-    def create_ball(self,cord_x,cord_y):
-        pygame.draw.rect(self.gamescreen, color3, (cord_x, cord_y, 25, 25))
-    def ball_move(self,cord_x,cord_y):
-        pygame.draw.rect(self.gamescreen, color3, (cord_x, cord_y, 25, 25))
-        pygame.display.flip()
-        print(cord_x,cord_y)
-    def update_screen(self,player_cord_x,player_cord_y,ball_cord_x,ball_cord_y):
+        self.player_racket.move_ip(cord_x,cord_y)
+        self.gamescreen.blit(self.cube, self.player_racket)
+        self.x = self.balls[0]
+        self.y = self.balls[1]
+        pygame.display.update()
+    def update_screen(self,ball_cord_x,ball_cord_y):
         self.gamescreen.blit(self.background, (0, 0))
-        pygame.draw.rect(self.gamescreen, color1, (player_cord_x, player_cord_y, 25, 100))
+        self.gamescreen.blit(self.cube, self.player_racket)
+        self.gamescreen.blit(self.cube2, self.bot_racket)
+        self.gamescreen.blit(text1, (100, 100))
         pygame.draw.rect(self.gamescreen, color3, (ball_cord_x, ball_cord_y, 25, 25))
 if __name__ =='__main__':
     player_cord_x = 35
@@ -42,7 +73,7 @@ if __name__ =='__main__':
     bot_cord_y = 250
     ball_x = 350
     ball_y = 250
-    ball = (ball_x,ball_y)
+    ball = (ball_x, ball_y)
     K_w = pygame.K_w
     K_s = pygame.K_s
     p = ping_pong()
@@ -50,38 +81,38 @@ if __name__ =='__main__':
     p.create_player(player_cord_x, player_cord_y)
     p.create_bot(bot_cord_x, bot_cord_y)
     p.create_ball(350,250)
-    p.player_move(35, 250)
-    x = int(random.randrange(-1, 2))
-    y = int(random.randrange(-1, 2))
-    balls_move = (x, y)
-    if balls_move == (0, 0):
-        x = int(random.randrange(-1, 2))
-        y = int(random.randrange(-1, 2))
-    balls_move = (x, y)
+    pmin_y = 250
+    pmax_y = 350
+    bmin_y = 250
+    bmax_y = 350
     while online:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 online = False
-        clock.tick(50)
-        pygame.display.flip()
+        clock.tick(160)
         keys = pygame.key.get_pressed()
         if keys[K_w]:
-            if player_cord_y <= 0:
-                move = (0,0)
+            if  player_cord_y<= 0:
+                p.player_move(0, 0)
+                pygame.display.update()
             elif player_cord_y > 0:
-                move = (0, -5)
-            player_cord_y += move[1]
-            player_racket = [(player_cord_x, player_cord_y)]
-            p.player_move(player_cord_x, player_cord_y)
+                player_cord_y+= -1
+                p.player_move(0, -1)
+                pmin_y += -1
+                pmax_y +=  -1
+                pygame.display.update()
         elif keys[K_s]:
             if player_cord_y >= 500:
-                move = (0, 0)
+                p.player_move(0, 0)
+                pygame.display.update()
             elif player_cord_y < 500:
-                move = (0, 5)
-            player_cord_y += move[1]
-            player_racket = [(player_cord_x, player_cord_y)]
-            p.player_move(player_cord_x, player_cord_y)
-        p.update_screen(player_cord_x, player_cord_y, ball_x,ball_y)
+                player_cord_y += 1
+                p.player_move(0, 1)
+                pmin_y += 1
+                pmax_y += 1
+                pygame.display.update()
+        pygame.display.flip()
+        p.update_screen(ball_x,ball_y)
         if ball_y >= 580:  # down wall
             if balls_move == (0, 1):
                 balls_move = (0, -1)
@@ -96,7 +127,28 @@ if __name__ =='__main__':
                 balls_move = (0, 1)
             if balls_move == (1, -1):
                 balls_move = (1, 1)
+        if ball_x <= 60:
+            if pmin_y<=ball_y <=pmax_y:
+                if balls_move == (-1,0):
+                    balls_move = (1,0)
+                if balls_move == (-1, 1):
+                    balls_move = (1, 1)
+                if balls_move == (-1, -1):
+                    balls_move = (1, -1)
+        print(bmin_y,bmax_y)
+        if ball_x >= 720:
+            if bmin_y<=ball_y <=bmax_y:
+                if balls_move == (1,1):
+                    balls_move = (-1,1)
+                if balls_move == (1, -1):
+                    balls_move = (-1, -1)
+                if balls_move == (1, 0):
+                    balls_move = (-1, 0)
         ball_x += balls_move[0]
         ball_y += balls_move[1]
-        p.ball_move(ball_x,ball_y)
+        bmin_y += balls_move[1]
+        bmax_y += balls_move[1]
+        print('ball:',ball_x,ball_y ,'\n','min , max :', bmin_y,bmax_y )
+        p.bot_move(ball_x,ball_y)
+        p.ball_move(balls_move[0],balls_move[1])
         pygame.display.update()
